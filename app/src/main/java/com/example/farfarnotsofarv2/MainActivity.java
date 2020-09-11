2a15c1b4ab04da75327453b62209d28e4185254d
CorentinLivain <corentinlivain.sti2d@gmail.com>
2020-09-11 22:59:39 +0200

    menu, bouton, barre cachée

--- app/src/main/java/com/example/farfarnotsofarv2/MainActivity.java
+++ app/src/main/java/com/example/farfarnotsofarv2/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.farfarnotsofarv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnJouer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void game(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
